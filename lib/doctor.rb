654f15eb22d6e17ecea0dd7360f5799da865e5d0
plai217 <philiplai217@gmail.com>
2015-11-16 16:12:29 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Doctor
+
+  attr_accessor :name, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    self.appointments << appointment
+    appointment.doctor = self
+  end
+
+  def patients
+    appointments.collect {|appointment| appointment.patient}
+  end
+
+end
