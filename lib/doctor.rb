7d47cf3408c0da2c5494da3a269f10b3c84232cc
github username <github email address>
2015-11-17 16:30:21 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,16 @@
+class Doctor
+  attr_accessor :name, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    appointments << appointment
+  end
+
+  def patients
+    appointments.collect { |appointment| appointment.patient }
+  end
+end
