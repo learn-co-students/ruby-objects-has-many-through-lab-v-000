290f510a3ea3d3a35187fc0b78f85a42aa89c916
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-12-23 19:03:08 -0500

    clarify tests remove let blocks, replace with local variables

--- spec/02_song_spec.rb
+++ spec/02_song_spec.rb
@@ -1,24 +1,30 @@
 require "spec_helper"
 
 describe "Song" do
-  let!(:rap) { Genre.new("rap") }
-  let!(:ninety_nine_problems) { Song.new("99 problems", rap) } 
-  let!(:jay_z) { Artist.new("Jay-Z") }
 
   describe "#new" do 
     it "initializes with a name and a genre" do 
+      rap = Genre.new("rap")
       expect{Song.new("Lucifer", rap)}.to_not raise_error
     end
   end
 
   describe "#genre" do
     it "belongs to a genre" do 
+      jay_z = Artist.new("Jay-Z")
+      rap = Genre.new("rap")
+      ninety_nine_problems = Song.new("99 Problems", rap)
+
       expect(ninety_nine_problems.genre).to eq(rap)
     end 
   end
 
   describe "#artist" do
     it "belongs to a artist" do 
+      jay_z = Artist.new("Jay-Z")
+      rap = Genre.new("rap")
+      ninety_nine_problems = Song.new("99 Problems", rap)
+
       ninety_nine_problems.artist = jay_z
       expect(ninety_nine_problems.artist).to eq(jay_z)
     end 
