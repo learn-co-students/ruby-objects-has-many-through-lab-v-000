2eaadc5097e8eb872f90f2fd27d52389d5983b69
Tsundu <osmentdan@gmail.com>
2016-01-03 16:32:14 -0500

    added objects with relationships

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song 
+ 
+attr_accessor :name, :artist, :genre 
+
+  def initialize(name, genre)
+
+    @name = name 
+    @genre = genre
+    self.genre.songs << self 
+
+  end 
+
+
+
+
+end 
