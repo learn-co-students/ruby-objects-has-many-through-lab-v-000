42dad93dcdc61b8b6528fd5065c50b81f7fa6b41
Adam King <adam.king0126@gmail.com>
2016-01-26 11:19:51 -0600

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,17 @@
+class Patient
+  attr_accessor :name, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def doctors
+    @appointments.map{|appointment| appointment.doctor}
+  end
+end
