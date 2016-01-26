335ad8cfa297bc97918e2e7fca169af1bd17a876
mystycs <mystycs@gmail.com>
2016-01-26 17:23:08 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+
+  attr_accessor :name, :songs, :genres
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+  end
+
+  def genres
+    self.songs.collect { |song| song.genre }
+  end
+
+end
