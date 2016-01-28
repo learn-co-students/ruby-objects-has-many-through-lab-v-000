16f1d2fd1bbfc280e8632e3026d0dfaf2ea05ef6
Smulligan85 <sean.mulligan85@gmail.com>
2016-01-28 06:42:07 -0800

    Add has many through associations

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre
+  attr_accessor :name, :songs
+  
+  def initialize(name)
+      @name = name
+      @songs = []
+  end
+  
+  def add_song(song)
+      @songs << song
+  end
+  
+  def songs
+      @songs
+  end
+  
+  def artists
+      @songs.collect { |song| song.artist }
+  end
+end
