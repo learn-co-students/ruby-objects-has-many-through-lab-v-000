32aaaf7266dafc6fb053484e31a10f16919f520c
Rafael A. Mejia <rafael@dataminded.net>
2016-02-03 04:33:34 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def songs
+    return @songs
+  end
+
+  def genres
+    return @songs.collect {|song| song.genre}
+  end
+
+
+end
