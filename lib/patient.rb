5949899bda1e7e6ba45e60d18d34f2bc2a4766c2
Lady Codington <myemailforcoding@gmail.com>
2016-02-08 20:45:29 -0500

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,16 @@
+class Patient
+  attr_accessor :name, :appointments, :doctors
+
+  def initialize(name)
+    @name = name
+    @appointments = Array.new
+    @doctors = Array.new
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor.patients << self
+    appointment.patient = self
+    @doctors << appointment.doctor
+  end
+end
