71811e555939fc340734ea8917fdbe9d7932d733
esimonian <eli.simonian@gmail.com>
2016-03-06 18:54:05 -0800

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,17 @@
+class Genre
+  attr_accessor :name, :songs
+ 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+ 
+  def songs
+    @songs
+  end
+ 
+  def artists
+    @songs.collect { |song|song.artist}
+    
+  end
+end
