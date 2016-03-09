cc72f9fe0dc3240f2c45153722684f453cd945f6
jsbenning <jbenninghoff1970@yahoo.com>
2016-03-09 21:12:28 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,32 @@
+class Genre
+  attr_accessor :name, :song, :artist
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    @songs.collect do |song|
+      song.artist
+    end
+  end
+
+end
+
+=begin
+
+* A genre should be initialized with a name and an empty `@songs` array.
+* The `Genre` class needs an instance method, `#add_song`, that takes in an argument of a song and adds that song to the genre's collection of songs. Now you can use the `Genre#add_song` method in the `Song` class' initialize method to tell the genre that it owns the given song.
+* The `Genre` class needs an instance method, `#songs`, that returns the `@songs` array.
+* The `Genre` class needs an instance method, `#artists`, that iterates over the genre's collection of songs and collects the artist that owns each song.
+
+=end
