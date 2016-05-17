beb045ea058fcd9d6e4d5067245a01278c1d5c4f
kaileeagray <kaileeagray@gmail.com>
2016-05-17 15:45:01 -0500

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,4 +8,15 @@ class Doctor
     @appointments =[]
   end
 
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+  def patients
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+
 end
