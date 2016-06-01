ea6185cc0d9b67e6b8c1cce6bc6a68043cd9b233
Dom McKellar <dom.mckellar@gmail.com>
2016-05-31 22:08:12 -0700

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+  attr_accessor :songs
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    songs << song
+    song.artist = self
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
+
+end
