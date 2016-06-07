e277ddd4eebdf10872cdf0b1dd42a27245f48faf
bmolina-nyc <bruce.d.molina@gmail.com>
2016-06-06 21:31:38 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,20 @@
+class Doctor 
+
+  attr_reader :name, :appointments 
+
+  def initialize(name)
+    @name = name 
+    @appointments = []  
+  end 
+
+  def add_appointment(appointment)
+    appointments << appointment 
+    appointment.doctor = self 
+  end
+
+  def patients 
+    appointments.map { |appointment| appointment.patient }
+  end
+
+
+end 
