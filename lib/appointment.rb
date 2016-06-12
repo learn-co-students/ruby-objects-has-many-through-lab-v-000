130a0db27032e824bc8265b76f53e0707620dfc1
Bob Habig <rfhab2@gmail.com>
2016-06-12 14:47:19 -0400

    Update appointment.rb

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,10 +2,10 @@ class Appointment
 
   attr_accessor :date, :doctor, :patient
 
-  def initialize(date, patient)
+  def initialize(date, doctor)
     @date = date
-    @patient = patient
-    patient.add_appointment(self)
+    @doctor = doctor
+    doctor.add_appointment(self)
   end
 
 end
