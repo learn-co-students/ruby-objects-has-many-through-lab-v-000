860735f3dff0218d6b4a0dafd3aaf1c60f1e263e
bjacooper <bjacooper@gmail.com>
2016-06-12 18:54:55 +0100

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,25 @@
+class Appointment
+
+  attr_accessor :date, :doctor, :patient
+
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+    doctor.add_appointment(self)
+  end
+
+
+  def patient
+
+  end
+
+  def doctor
+
+  end
+
+
+
+
+
+
+end
