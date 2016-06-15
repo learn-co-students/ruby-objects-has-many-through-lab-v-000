01028ff3752170aa9c7d5f177437d7e460a05fca
Caleb Faught <faughtcj@gmail.com>
2016-06-15 01:03:53 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,19 @@
+class Patient
+  attr_accessor :name, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    self.appointments << appointment
+    appointment.patient = self
+  end
+
+  def doctors
+    self.appointments.collect do |appointment|
+      appointment.doctor
+    end
+  end
+end
