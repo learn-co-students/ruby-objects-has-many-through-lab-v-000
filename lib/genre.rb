20c51f061166d4af878495b78c93107875af7cca
Keith Wellman <keith.wellman@icloud.com>
2016-07-06 02:02:19 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,18 @@
+class Genre
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def artists
+    self.songs.collect do |song|
+      song.artist
+    end
+  end
+end
