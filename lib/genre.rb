5af0f992adb0f27c7dc970dda79c3daef00a6a4e
Sammy Wu <wu.sammy@gmail.com>
2016-08-29 21:58:41 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,18 @@
+class Genre
+
+  attr_accessor :name, :songs, :artists
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    self.songs.collect {|song| song.artist}
+  end
+
+end
