4a5e703b3837c5c839cff63dc702729c13634d0b
vanita eckert <vanita112003@gmail.com>
2016-10-11 16:23:14 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,24 @@
+require "pry"
+class Genre
+
+attr_accessor :songs
+attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def artists
+    #  binding.pry
+    self.songs.collect do |song|
+      song.artist
+    end
+  end
+
+
+end
