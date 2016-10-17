337247de1b315336eec0c709de743d1499691c7d
Adam Friedl <ajfriedl@gmail.com>
2016-10-16 20:34:53 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+	attr_accessor :name, :genre, :artist
+
+	def initialize(name, genre)
+		@name = name
+		@genre = genre
+		genre.add_song(self)
+	end
+
+	def add_artist(artist)
+		artist.songs << self
+	end
+end
