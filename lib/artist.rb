dcb5630f1a50ebf9973fbe655697e21a02bf9ab2
Teresa Grosch <tgrosch91@gmail.com>
2016-11-13 21:48:58 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+
+attr_accessor :name
+
+attr_reader :songs, :genres
+
+def initialize(name)
+	@name=name
+	@songs = []
+end
+
+def add_song(song)
+	self.songs << song
+	song.artist = self
+end
+
+def genres
+	self.songs.collect do |song|
+		song.genre
+	end
+end
+
+end
---
+++ lib/doctor.rb
@@ -0,0 +1,22 @@
+class Doctor
+
+attr_accessor :name, :appointments, :patients
+
+def initialize(name)
+	@name = name
+	@appointments = []
+end
+
+def add_appointment(appointment)
+	self.appointments << appointment
+	appointment.doctor = self
+end
+
+def patients
+	self.appointments.collect do |appointment|
+		appointment.patient
+	end
+end
+
+
+end
