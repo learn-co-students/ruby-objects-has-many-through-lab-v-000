be3d388c89c60c9a1ad51d457fb337f020a1b5ac
Emmanuel Owusu <e_owusu@live.com>
2016-11-28 04:05:10 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+
+  attr_accessor :name, :genre
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @genre = genre
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    self.songs.collect do |song|
+      song.genre
+    end
+  end
+
+
+end
