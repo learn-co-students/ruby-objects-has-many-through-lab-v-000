212a4b4e9de04dd2a669465a98745bf9721f8028
Matt Borg <msimonborg@gmail.com>
2016-12-27 07:02:08 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+  end
+
+  def genres
+    self.songs.map { |song| song.genre }
+  end
+end
