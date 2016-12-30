499ea746d12d8388de458b21f5f7db1f2290f195
Alexis Roman <alexismmroman21@gmail.com>
2016-12-30 01:24:51 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name 
+    @songs = []
+    
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    
+  end
+
+  def genres
+
+    self.songs.collect do |song|
+      song.genre
+      
+    end
+    
+  end
+  
+end
