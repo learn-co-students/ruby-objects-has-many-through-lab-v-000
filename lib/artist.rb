1262bd7a6d4e43c683075f206ce9acec0ba04f1b
Kevin Adkins <kevinladkins@gmail.com>
2017-01-28 19:58:17 +0000

    built #initialize and #add_song; 2 tests passing

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+class Artist
+
+attr_reader :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+end
