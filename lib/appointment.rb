565d016e0b7b568c36764918596619cc3d10bfa5
Kevin Adkins <kevinladkins@gmail.com>
2017-01-29 17:51:10 +0000

    all test now passing

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,13 +1,11 @@
 class Appointment
 
-  attr_accessor :doctor
+  attr_accessor :doctor, :patient, :date
 
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
-    doctor.add_appointment(date)
+    doctor.add_appointment(self)
   end
-
-
 
 end
