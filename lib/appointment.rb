f4647f17c4ab451e85da8a741931543fe292cb53
Annette  Drapalski <annette.drapalski@gmail.com>
2017-02-09 23:38:43 +0000

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,11 +3,10 @@ class Appointment
 
   attr_accessor :date, :doctor, :patient
 
-def initialize(date, doctor)
-  @date = date
-  @doctor = doctor
-  @patient = patient
-  doctor.add_appointment(self)
-end
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+    doctor.add_appointment(self)
+  end
 
 end
