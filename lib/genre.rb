7a45a39593a6c07ffdd9e3e0fdcf6189dfedbf4b
Kristi Thompson <kristiforpresident@gmail.com>
2017-02-16 18:22:24 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,21 @@
+class Genre
+  attr_accessor :name
+  attr_reader :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.genre = self
+  end
+
+  def artists
+    self.songs.collect do |song|
+      song.artist
+    end
+  end
+
+end
