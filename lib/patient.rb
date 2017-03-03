e4a7a795ecca7ffac2de4d4bc5a53d2415f51d2d
David Wistocki <david@davidwistocki.com>
2017-03-03 23:23:14 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,20 @@
+require "pry"
+
+class Patient
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+    # binding.pry
+  end
+
+  def doctors
+    
+  end
+end
