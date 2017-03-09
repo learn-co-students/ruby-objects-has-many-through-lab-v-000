83b24c92e10af344e3ce98a8558d17af97a13e31
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-03-09 16:24:14 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,23 @@
+class Patient
+
+  attr_accessor :name, :doctor, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def doctors
+    self.appointments.collect {|appointment| appointment.doctor}
+  end
+
+end
