0d388b8839b2c0f773783556392bd6486b3bb79d
Bryan Hou <bryan.hou94@gmail.com>
2017-04-02 16:42:30 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+  attr_reader :name, :songs
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def genres
+    @songs.collect{ |song| song.genre}
+  end
+end
