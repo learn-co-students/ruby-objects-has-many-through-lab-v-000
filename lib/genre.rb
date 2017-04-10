728540e9c2e58ae5b4018e8db99a92a74c83dc2f
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-10 22:34:03 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,14 @@
+class Genre
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+  attr_accessor :name, :genre
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+  end
+
+end
