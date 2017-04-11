a73e7043bcfc5c7da584045d887fe7efd394f528
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-11 01:12:37 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,10 +8,17 @@ class Artist
 
   def add_song(song)
     @songs << song
+    song.artist = self
   end
 
   def songs
     @songs
+  end
+
+  def genres
+    self.songs.collect do |song|
+      song.genre
+    end
   end
 
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,10 @@
 class Song
-  attr_accessor :name, :genre
+  attr_accessor :name, :artist, :genre
 
   def initialize(name, genre)
-    @name = name
-    @genre = genre
+      @name = name
+      @genre = genre
+      genre.add_song(self)
   end
 
 end
