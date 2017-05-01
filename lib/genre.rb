8054b073f62f884f1513806e1c69959a8fc08e88
Nick Bair <bairinc@gmail.com>
2017-05-01 17:04:54 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,17 @@
+class Genre
+  attr_accessor :songs
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def artists
+    @songs.collect { |song| song.artist }
+  end
+end
