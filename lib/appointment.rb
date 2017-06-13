b047cc744ae4c5a89a414047109a413b86f6cc73
Mel Chang <melanie.a.chang@gmail.com>
2017-06-13 19:40:11 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+class Appointment
+
+  attr_accessor :appointment, :doctor, :patient
+
+  def initialize(appointment, doctor)
+    @appointment = appointment
+    @doctor = doctor
+    doctor.add_appointment(self)
+  end
+
+
+
+end
