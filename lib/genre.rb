d7195e1601494890bad6c866c9b44f71b5281d83
Anthony McCormick-Gonzales <a.j.mccormickgonzales@gmail.com>
2017-06-13 20:57:02 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,17 @@
+class Genre
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.genre = self
+  end
+
+  def artists
+    @songs.collect {|song| song.artist}
+  end
+end
