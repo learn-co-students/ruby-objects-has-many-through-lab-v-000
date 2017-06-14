3df1f8e2fc96b86c65d89c895da8d2fa71890d8f
Denise Dearman <denisedearman@Patricks-MacBook-Pro.local>
2017-06-14 11:14:00 -0400

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,12 @@
+class Appointment
+  attr_accessor :doctor, :patient, :date
+
+  def initialize (date, doctor)
+    @date = date
+    @doctor = doctor
+    @doctor.add_appointment(self)
+  end
+
+
+
+end
