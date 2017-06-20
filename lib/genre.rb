e41cbceb1d123c539b4a153fe08922d81cb952ea
Timothy Ege <retibra@gmail.com>
2017-06-20 14:52:33 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,19 @@
+class Genre
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    @songs.collect do |song|
+      song.artist
+    end
+  end
+
+end
