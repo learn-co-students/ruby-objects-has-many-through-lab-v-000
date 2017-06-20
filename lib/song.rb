15f91a42b41bf2f8793492ec8a9f077f273d65e7
Paul Snider <pbsnider@gmail.com>
2017-06-20 18:34:29 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+    self.genre.add_song(self)
+    #self.genre = genre
+  end
+end
