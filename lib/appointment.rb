495feb8031ca1b263c5cbec353d2e346bb6a3366
Annie Bourke <aabourke@gmail.com>
2017-06-21 04:12:31 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,10 @@
+class Appointment
+  attr_accessor :name, :doctor, :patient
+
+  def initialize(doctor, patient)
+    @doctor = doctor
+    @patient = patient
+    patient.add_appointment(self)
+  end
+  
+end
