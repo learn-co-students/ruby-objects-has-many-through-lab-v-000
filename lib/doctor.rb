86ad66cc986093a36299fb6b59c6bd48e750d317
veloblank <veloblank@gmail.com>
2017-06-23 22:45:44 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment=(appointment)
+    @appointment = appointment
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def patients
+    self.appointments.collect {|var| var.patient}
+
+  end
+
+
+end
