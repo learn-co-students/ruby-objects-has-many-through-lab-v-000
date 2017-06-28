1453186c97ec6c8d89c658e0f4c6e5ad737533ed
kyleblee <hello.kylelee@gmail.com>
2017-06-28 08:53:58 -0400

    lab complete

---
+++ lib/genre.rb
@@ -0,0 +1,18 @@
+class Genre
+  attr_accessor :name
+  attr_reader :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.genre = self
+  end
+
+  def artists
+    self.songs.collect {|song| song.artist}
+  end
+end
