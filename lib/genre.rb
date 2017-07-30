a9f81b6abdab751f21cebab031447be16d4b85c4
Tyler <tbuchhei@alumni.nd.edu>
2017-07-30 13:05:46 -0500

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,19 @@
+class Genre
+
+  attr_accessor :songs
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    songs << song
+  end
+
+  def artists
+    songs.map {|song| song.artist}
+  end
+
+end
