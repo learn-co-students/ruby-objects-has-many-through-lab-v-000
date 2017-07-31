992becf4e79681a3145aa8bfbf9cf756916810d6
Mariel F <marielbeth@gmail.com>
2017-07-31 17:30:06 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  attr_accessor :songs, :name
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def genres
+    @songs.collect do |song|
+      song.genre
+    end
+  end
+end
