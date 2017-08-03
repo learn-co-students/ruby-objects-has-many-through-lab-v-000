2364e0a6293238008a6a0002825d2976f812b033
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-08-03 20:50:44 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,8 +1,13 @@
 class Genre 
   attr_accessor :name
   
-  def initialize(name )
+  def initialize(name)
     @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    self.songs << song
   end
   
 end
