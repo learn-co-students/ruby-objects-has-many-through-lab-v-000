a74bb9bf69fdbff8e668feed5c530e34410cc3c6
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-08-09 14:43:14 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,8 @@
+class Appointment
+    attr_accessor :patient, :doctor, :date
+    def initialize(date, doctor)
+        @doctor = doctor
+        @date = date
+        doctor.add_appointment(self)
+    end
+end
