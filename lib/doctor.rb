3a2f218495479c63c880a683cc1fbcb29cdc0fa9
Wendy Lau <wlau903@gmail.com>
2017-09-11 18:33:37 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,22 @@
+class Doctor
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def add_appointment(appointment)
+    appointments << appointment
+    appointment.doctor = self
+  end
+
+  def patients
+    appointments.collect { |appointment| appointment.patient }
+  end
+
+end
---
+++ lib/patient.rb
@@ -0,0 +1,22 @@
+class Patient
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def add_appointment(appointment)
+    appointments << appointment
+    appointment.patient = self
+  end
+
+  def doctors
+    appointments.collect { |appointment| appointment.doctor }
+  end
+
+end
