25d76967214ba83f124fa72195dbd966c53a6d11
Sofia Candiani <sofia.candiani123@gmail.com>
2017-09-14 22:28:09 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+
+  attr_accessor  :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(name)
+    self.songs << name
+    name.artist = self
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    self.songs.map {|s| s.genre}
+  end
+
+end
