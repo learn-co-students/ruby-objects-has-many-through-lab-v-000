27dd1a579e3d46621d2fd1b0efba9bc03d2f7fa0
Christopher Harvey <chharvey@icloud.com>
2017-10-03 21:08:56 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,12 @@
+class Genre
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def artists
+    @songs.collect {|song| song.artist }.uniq
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+
+  attr_accessor :artist, :genre, :name
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+    genre.songs << self
+  end
+end
