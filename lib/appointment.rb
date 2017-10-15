2d5a9642f2688112f8b9be40280712fd686d4717
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-15 05:41:05 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Appointment
+  attr_accessor :date, :patient, :doctor
+
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+    doctor.add_appointment(self)
+  end
+
+
+end
