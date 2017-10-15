c5e9f01e0a636d0317b0e7153cd9f48f99d59a7b
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2017-10-15 23:24:13 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,11 @@
+class Appointment
+# #
+  attr_accessor :date, :doctor, :patient
+# #
+  def initialize(date, patient)
+    @date = date
+    @patient = patient
+    patient.add_appointment(self)
+  end
+# 
+end
