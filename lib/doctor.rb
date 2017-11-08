53ac8bd7a1c583c57705e987d14b7ca60f0a4f2f
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 05:02:50 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,23 @@
+class Doctor
+  attr_accessor :name, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def patients
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+end
