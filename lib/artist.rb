174115de136fc059e10eda17b03b9ef1173293a8
Min Kim <mk4555@nyu.edu>
2017-11-09 19:13:35 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def genres
+    @songs.collect do |song|
+      song.genre
+    end
+  end
+
+end
