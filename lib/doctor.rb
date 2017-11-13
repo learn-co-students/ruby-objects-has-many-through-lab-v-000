480830bc48323d7b00bd15b534890c3eabc4592e
Jess Mitchell <jessicamitch42@gmail.com>
2017-11-13 21:27:01 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,18 @@
+class Doctor
+  attr_accessor :name, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(app)
+    @appointments << app
+    app.doctor = self
+  end
+
+  def patients
+    self.appointments.collect{ |app| app.patient }
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre
+  # A genre should be initialized with a name and an empty @songs array.
+  # The Genre class needs an instance method, #add_song, that takes in an argument of a song and adds that song to the genre's collection of songs. Now you can use the Genre#add_song method in the Song class' initialize method to tell the genre that it owns the given song.
+  # The Genre class needs an instance method, #songs, that returns the @songs array.
+  # The Genre class needs an instance method, #artists, that iterates over the genre's collection of songs and collects the artist that owns each song.
+  attr_accessor :name
+  attr_reader :artist, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    @songs.collect{ |song| song.artist }
+  end
+
+end
