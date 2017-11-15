4f70ce5226ad3b05e76c6f3cbbff2c4aa5f77f05
Christopher DiStasio <github email address>
2017-11-15 14:01:55 -0600

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,7 +5,7 @@ class Appointment
     def initialize(date, doctor)
         @date = date
         @doctor = doctor
-        patient.add_appointment(self)
+        doctor.add_appointment(self)
     end
 
     def doctor
