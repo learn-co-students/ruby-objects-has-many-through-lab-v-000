55833ca63670acdf9c5235dd4d4794b4db27641e
Miwa Hirano <paintfx@gmail.com>
2017-11-17 19:51:31 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,19 @@
+class Patient
+  attr_accessor :name
+  attr_reader :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def doctors
+    self.appointments.collect {|appt| appt.doctor}
+  end
+
+end
