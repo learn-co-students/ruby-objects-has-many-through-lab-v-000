365c5bcb43bb010c41c849a2bb70b213f1bca0f9
Nolan Byrnes <nolanehs@gmail.com>
2017-11-25 20:14:31 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+attr_accessor :name, :songs
+
+def initialize(name)
+  @songs = []
+end
+
+
+
+
+end
+
+#An artist is initialized with a name and an empty @songs array.
+#The Artist class needs an instance method, #add_song, that takes in an argument of a song and adds that song to the artist's collection of songs. The #add_song method should also tell the song that it belongs to that artist.
+#The Artist class needs an instance method, #songs, that returns the @songs array.
+#The Artist class needs an instance method, #genres that iterates over that artist's songs and collects the genre of each song.
