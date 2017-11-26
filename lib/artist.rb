77e2908502efdcaec9e2ed603e0508c372f0e35d
Nolan Byrnes <nolanehs@gmail.com>
2017-11-26 00:30:08 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,25 @@
 class Artist
-attr_accessor :name, :songs
+attr_accessor :name, :songs, :genres
 
 def initialize(name)
+@name = name
   @songs = []
 end
 
+def add_song(song)
+@songs << song
+song.artist = self
+end
 
+def songs
+  @songs
+end
 
+def genres
+self.songs.collect do |song|
+  song.genre
+end
+end
 
 end
 
