730b31b30d9b95f165156d6f19dfaff4d9df194c
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-27 23:38:15 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class Genre
+  attr_accessor :name
+  attr_reader :songs
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def artists
+    self.songs.map {|song| song.artist}
+  end
+
+end
