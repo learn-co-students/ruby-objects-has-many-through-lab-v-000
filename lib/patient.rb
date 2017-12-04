dce540b517b456648ff9c0d6af109bbbaeb60411
Liz Burton <liz.burton147@gmail.com>
2017-12-04 19:08:22 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,13 @@
 class Patient
+attr_accessor: appointments
+attr_reader: name
 
+def initialize(name)
+  @name = name
+  @appointments = []
+end
+
+def add_appointment(appointment)
+  @appointments << appointment
+  appointment.patient = self
 end
