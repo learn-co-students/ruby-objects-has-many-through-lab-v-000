fc7563fbaf943245202d5d56f7f4da605f5b5728
charityyoungblood <youngblood.charity@gmail.com>
2017-12-06 14:51:52 -0800

    added attr_accessor and instance variables to song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,12 @@ require 'pry'
 
 # create Song class 
 class Song
+# create attr_accessor to access data of name and genre 
+  attr_accessor :name, :genre
 # create initialize method w/(name , genre) arguments
   def initialize(name, genre) 
-    
+    @name = name
+    @genre = genre
   end 
 # create genre method - no arguments 
   # - this method will display the genre type 
