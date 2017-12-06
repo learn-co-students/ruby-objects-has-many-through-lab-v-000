84909a082bac5439c4574951930d32f24ac9f72f
charityyoungblood <youngblood.charity@gmail.com>
2017-12-06 14:44:39 -0800

    added attr_accessor for name in genre.rb

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,13 +2,19 @@ require 'pry'
 
 # create Genre class 
 class Genre
+# create attr_accessor to access name
+  attr_accessor :name 
 # create initialize method w/(name) argument
+  def initialize(name)
   # - initialize with empty collection of songs
-
-# create name method - no arguments 
+    @name = name
+    @songs = []
+  end 
+# create name method - no argument - this is above using attr_accessor :name
   # - this method will display the name 
 
-# create songs method - no arguments 
+# create songs method - no arguments
+  
   # - this method will display the songs array contents
 
 # create artists method - no arguments 
