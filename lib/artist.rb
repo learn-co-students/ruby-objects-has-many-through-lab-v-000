be6f7cfc34785b471cac4c242ac20a874bbcd811
charityyoungblood <youngblood.charity@gmail.com>
2017-12-06 13:53:36 -0800

    added placeholder for add_songs(song) method in artist.rb

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,10 +12,11 @@ require 'pry'
     end 
 # create name method - no arguments 
     # - shown above in attr_accessor list 
-# create add_song method - no arguments 
+# create add_song method w/(song) argument 
+    def add_song(song)
   # - this method will add a new song instance to the @songs array
   # - this method will tell the song instance that it belongs to the artist instance 
-
+    end 
 # create songs method - no arguments 
   # - displays what songs are included in the songs array
 
