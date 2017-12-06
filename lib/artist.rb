89a50e625defa9881b9a8a6d0f403456bea0b452
charityyoungblood <youngblood.charity@gmail.com>
2017-12-06 13:44:00 -0800

    added comments on how to structure code

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,12 @@
 require 'pry' 
+
+# create an Artist class 
+
+# create initialize method w/name argument 
+  # - initialize with empty array of songs
+# create name method - no arguments 
+
+# create add_song method 
+  # - this method will add a new song instance to the @songs array
+  # - this method will tell the song instance that it belongs to the artist instance 
+
