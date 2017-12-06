ec49a9bc4dcb143513fe13ac12e1999ff04a11fc
charityyoungblood <youngblood.charity@gmail.com>
2017-12-06 13:50:36 -0800

    added placeholder for name method

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,9 @@ require 'pry'
       @songs = []
     end 
 # create name method - no arguments 
-
+    def name
+      
+    end 
 # create add_song method - no arguments 
   # - this method will add a new song instance to the @songs array
   # - this method will tell the song instance that it belongs to the artist instance 
