4e0cbf22f7ad944932d7cb628ed09f73a53c5678
Charity <Youngblood.charity@gmail.com>
2017-12-07 12:33:09 -0800

    added placeholder for Appointment class

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,16 @@
 require 'pry'
+
+# create Appointment class 
+class Appointment 
+# initialize with date and doctor instance
+
+# create patient method 
+  # - this method will show the relationship between  patient and appointment 
+  # - we do this by setting the instance = attr_accessor 
+
+# create doctor method 
+  # - this method will show the relationship between doctor and appointment 
+
+  
+  
+end 
