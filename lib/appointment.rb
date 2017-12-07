77882997e3d6dd38782f5ddb61db5ffd917f6352
Charity <Youngblood.charity@gmail.com>
2017-12-07 12:37:00 -0800

    added instance variables for date and doctor

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,10 @@ require 'pry'
 # create Appointment class 
 class Appointment 
 # initialize with date and doctor instance
-
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor 
+  end 
 # create patient method 
   # - this method will show the relationship between  patient and appointment 
   # - we do this by setting the instance = attr_accessor 
