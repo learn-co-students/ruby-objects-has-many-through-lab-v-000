36e67e7876ba3421dc1ba9e8a36dcd4d92b43ba1
Charity <Youngblood.charity@gmail.com>
2017-12-07 12:22:42 -0800

    added placeholder for add_appointment method

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -9,11 +9,14 @@ class Doctor
     @name = name
     @appointments = [] # when will you be adding appointments to this array? 
   end 
-# create name method - may be able to use attr_accessor 
-
-# create add_appointment method 
+# create name method - above in attr_accessor 
+  
+# create add_appointment method w/(appointment) argument
+  def add_appointment(appointment)
   # - this method will show th appointment belongs to the doctor 
   # - this is shown by giving a Doctor instance an appointment attr_accessor. Then we set that appointment attribute to an instance of the Doctor  class.
+    
+  end 
 
 # create appointments method - this should display the contents of the appointments array 
 
