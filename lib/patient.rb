868dbf0de51a2d6e5ca15b3cc1f442e7b517e380
Charity <Youngblood.charity@gmail.com>
2017-12-07 12:44:51 -0800

    added placeholder for add_appt method

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,10 +2,16 @@ require 'pry'
 
 # create Patient class 
 class Patient
+# create attr_accessor for name
+  attr_accessor :name 
 # intialize with name 
-
+  def initialize(name)
+    @name = name
+  end 
 # create add_appointment method 
-
+  def add_appointment(appointment)
+    
+  end 
 # create doctors method 
   
   
