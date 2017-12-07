ea222a98ecb473d356f0fbb31b4bd0b37dc63c10
Charity <Youngblood.charity@gmail.com>
2017-12-07 13:07:40 -0800

    added @appointments array in order to access patient/appt relationship

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -7,11 +7,16 @@ class Appointment
 # initialize with date and doctor instance
   def initialize(date, doctor)
     @date = date
-    @doctor = doctor 
+    @doctor = doctor
+    @appointments = []
   end 
 # create patient method 
+  def patient
   # - this method will show the relationship between  patient and appointment 
+    # where are appointments and patients connected? in the @appointment array
+    binding.pry
   # - we do this by setting the instance = attr_accessor 
+  end 
 
 # create doctor method 
   # - this method will show the relationship between doctor and appointment 
