4b12fbb82ac660d26153ab38758b348fea4f7ea2
Charity <Youngblood.charity@gmail.com>
2017-12-07 12:50:35 -0800

    added comments to doctor method in patient.rb

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,7 +3,7 @@ require 'pry'
 # create Patient class 
 class Patient
 # create attr_accessor for name
-  attr_accessor :name 
+  attr_accessor :name, :appointments
 # intialize with name 
   def initialize(name)
     @name = name
@@ -13,8 +13,15 @@ class Patient
 # create add_appointment method 
   def add_appointment(appointment)
     @appointments << appointment
+    appointment.patient = self
+    # set appointment instance to attr_accessor
   end 
 # create doctors method 
+  def doctors 
+    # how are doctors and appts connected? 
+    # they are connected through the @appts array 
+    binding.pry
+  end 
   
   
 end 
