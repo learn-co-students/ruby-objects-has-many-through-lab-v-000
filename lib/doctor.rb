23f6460e07f7e9c0465253183d5875df85c956d7
Charity <Youngblood.charity@gmail.com>
2017-12-07 12:01:53 -0800

    added comments on how to structure code in doctor.rb

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,17 @@
 require 'pry' 
 
-# 
+# create Doctor class
+
+# initialize method w/name and @appointments array = []
+
+# create name method - may be able to use attr_accessor 
+
+# create add_appointment method 
+  # - this method will show th appointment belongs to the doctor 
+  # - this is shown by giving a Doctor instance an appointment attr_accessor. Then we set that appointment attribute to an instance of the Doctor  class.
+
+# create appointments method - this should display the contents of the appointments array 
+
+# create patients method 
+  # - this method will show the relationship between patients and appointments 
+
