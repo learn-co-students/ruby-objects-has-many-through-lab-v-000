4fabd68ced797123519247c45a2f2755fd9a4e1f
Alyssa Clair <clairaly13@gmail.com>
2017-12-08 21:44:29 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+    @genre.add_song(self)
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+end
