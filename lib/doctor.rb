e38128e56fa1892231ddee992735c0e7ce23a511
Bryan Hooper <bryan@lindsayfarm.net>
2017-12-12 11:12:43 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+# Doctor Class
+#
+
+class Doctor
+
+  attr_accessor :name, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def patients
+    @appointments.map { |appointment| appointment.patient }
+  end
+
+
+
+end
+
+
+
+
