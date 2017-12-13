328be2a55ad2285c5dd1df6c8ba6d0a1562aa178
Laura Bodell <bodelllaura@gmail.com>
2017-12-13 23:38:48 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+
+  attr_accessor :name
+
+def initialize(name)
+  @songs = []
+  @name= name
+end
+
+def add_song(song)
+@songs << song
+song.artist = self
+end
+
+def songs
+  @songs
+end
+
+def genres
+  songs.collect do |song|
+    song.genre
+  end
+end
+
+
+
+end
