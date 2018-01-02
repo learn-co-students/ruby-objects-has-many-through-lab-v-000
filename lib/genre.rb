5d6f8ccc61026588f7aa8ae18b8dd2f34416bd2b
TravisA <github email address>
2018-01-01 20:32:25 -0700

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,19 @@
+class Genre
+  attr_accessor :name, :songs, :artists
+
+  def initialize(name)
+  	@name = name
+  	@songs = []
+  end
+
+  def add_song(song)
+  	@songs << song
+  	song.genre = self
+  end
+
+  def artists
+  	self.songs.map do |song|
+  	  song.artist
+  	end
+  end
+end
