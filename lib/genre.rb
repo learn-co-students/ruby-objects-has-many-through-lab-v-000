9a8bfdd1234b59d687d4c7b21f7ff830f29fb2bd
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-10 17:47:50 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,22 @@
+class Genre
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    self.songs.collect {|songs| songs.artist}
+  end
+
+end
