070c2bb09a1f6c5a258c0aa6b47e3a326a1a427b
Anaxirinia Jimenez <anax131@gmail.com>
2018-01-24 06:13:56 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient
+  attr_accessor :name, :appointment
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def doctors
+    #binding.pry
+    self.appointments.collect do |appointment|
+      appointment.doctor
+    end
+  end
+
+
+end
