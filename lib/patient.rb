e4e1c09d69a0f7f827f7589479996fbd1477f6ba
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-30 21:07:39 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,6 +16,6 @@ class Patient
   end
 
   def doctors
-    @appointments.collect { |appointment| appointment.doctor }
+    self.appointments.collect { |appointment| appointment.doctor }
   end
 end
