5f3822664b681d515dac047d3fe51a3dc90ccc98
github username <github email address>
2018-02-07 16:41:18 -0500

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Patient
+  attr_accessor :name, :appointment
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def doctors
+    self.appointments.collect do |appointment|
+      appointment.doctor
+    end
+  end
+end
