8a7bc69ad6aee5115cfffc3015c8f7c260a2f917
Jayon Kim <jpkim921@gmail.com>
2018-03-03 18:16:32 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+class Appointment
+  attr_accessor :patient, :doctor, :date
+
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+    doctor.add_appointment(self)
+  end
+
+
+
+
+end
