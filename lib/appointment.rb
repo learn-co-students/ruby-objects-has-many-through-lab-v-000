d779784a0ea7e11856ef027ba433085bc8702527
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 17:40:41 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,9 @@
+class Appointment
+  attr_accessor :doctor, :patient, :name 
+
+  def initialize(name, patient)
+    @name = name 
+    self.patient = patient  
+    patient.add_appointment(self) 
+  end 
+end
