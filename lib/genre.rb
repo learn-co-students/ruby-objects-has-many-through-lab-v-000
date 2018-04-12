ed2af0473f63539c52f58acf741e3164ab225069
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-04-12 05:43:24 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Genre
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+     songs << song
+  end
+
+  def artists
+    songs.collect {|song| song.artist}
+    #binding.pry
+  end
+end
