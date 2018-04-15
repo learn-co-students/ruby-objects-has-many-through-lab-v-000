af0b740f3149697b5c4e9b553ac0de93b1fb4cd8
ecssiah <ecssiah@gmail.com>
2018-04-14 21:07:27 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  attr_reader :name
+  attr_accessor :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def genres
+    @songs.collect {|song| song.genre}
+  end
+end
