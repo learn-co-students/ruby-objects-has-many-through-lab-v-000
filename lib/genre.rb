7bf5713acb988abe8f2ea6eca9f07b7d3b60b183
Nicholas Bharucha <nickbharucha@ou.edu>
2018-04-22 02:25:31 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,17 @@
+class Genre
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def artists
+    songs.map{|s| s.artist}
+  end
+
+end
