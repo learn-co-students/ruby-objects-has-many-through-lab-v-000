7db36df7c6b114edbc505568a9f8bd794de1812a
Michael Blandford <mdblandford@gmail.com>
2018-05-02 19:40:37 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,9 @@
+class Appointment
+  attr_accessor :date, :doctor , :appointments, :patient
+  
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+    doctor.add_appointment(self)
+  end
+end
