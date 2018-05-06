c12e2cc9885b8b6e6eb771ddb28b16eaf3fdad94
ERIC KELLY <eakelly220@gmail.com>
2018-05-06 21:23:36 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,12 @@
+class Appointment 
+  attr_accessor :name, :patient, :doctor
+  
+  def initialize(name,patient)
+    @name = name 
+    @patient = patient 
+    patient.add_appointment(self)
+  end 
+  
+  
+end 
+
