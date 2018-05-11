30effb8b7f3898bc6826ab0ed33fcd6984d2512f
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-11 02:01:52 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,7 @@
+
+class Doctor
+
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+
+class Song
+  attr_accessor :title, :genre, :artist
+
+  def initialize(title, genre)
+    @title = title
+    @genre = genre
+    genre.songs << self
+  end
+
+end
