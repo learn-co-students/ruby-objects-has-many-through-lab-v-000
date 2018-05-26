73d351f648d0bbf1fee58aa9f0f893094aa35d3d
Josh Cesana <joshuacesana@gmail.com>
2018-05-26 23:19:38 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor 
+  @@all = []
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end 
+  
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self 
+    end 
+  end 
+  
+  def patients 
+    appointments.map do |appointment|
+      appointment.patient
+    end 
+  end 
+  
+end 
