fc0fa9480e103609372e0611b94023375e3a7d2a
Richard Cho <richardcho100@gmail.com>
2018-05-27 05:16:01 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment
+  attr_reader :patient, :doctor, :date
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
+  end
+end
--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,27 @@
+require "pry"
 class Artist
+  attr_accessor :name
 
-  
-  def initialize
+  @@all = []
 
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    songs.collect {|object| object.genre}
   end
 end
