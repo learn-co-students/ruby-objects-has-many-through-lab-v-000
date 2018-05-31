00aa2f384531ac7cac07d0a07731dd746546ce9a
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-31 20:55:34 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,18 @@
+class Appointment
+
+  attr_accessor :date, :doctor, :patient
+  
+  @@all = []
+
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
