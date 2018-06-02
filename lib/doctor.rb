454947d14cfcf8c5af37a9e6335004c9eb446cd8
Emily Welsch <emwelsch@gmail.com>
2018-06-02 18:00:24 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,8 +22,5 @@ class Doctor
   def patients
     Appointment.all.collect {|appointment| appointment.patient}
   end
-# The Doctor class needs an instance method, #patients,
-# that iterates over that doctor's appointments and collects
-# the patient that belongs to each appointment.
 
 end
