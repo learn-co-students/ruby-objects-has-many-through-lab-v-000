86f4b1d156f3a5a62ed3c56f1b041c3476e29dd5
Christia T <christiatidwell@bellsouth.net>
2018-06-07 02:23:13 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+
+class Artist
+  attr_accessor :name
+
+@@all = []
+
+  def initialize(name)
+      @name = name
+
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def new_song(genre, name)
+      Song.new(genre, self, name)
+    end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
+
+  end
---
+++ lib/doctor.rb
@@ -0,0 +1,31 @@
+class Doctor
+
+  attr_accessor :name
+@@all_doctors = []
+
+  def initialize(name)
+    @name = name
+
+    @@all_doctors << self
+  end
+
+  def self.all
+    @@all_doctors
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.select do |appointment|
+    appointment.doctor == self
+    end
+  end
+
+  def patients
+    self.appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+end
