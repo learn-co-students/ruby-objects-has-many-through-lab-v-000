98fedee38617d90beb411a6b6027a28ca76824a2
Christia T <christiatidwell@bellsouth.net>
2018-06-07 03:16:21 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
   end
 
   def new_appointment(doctor= "The Doctor", date= "Friday, January 32nd")
-    self.appointment.new(patient= "Devin Townsend", doctor, date)
+    self.new(patient= "Devin Townsend", doctor, date)
   end
 
   def doctors
