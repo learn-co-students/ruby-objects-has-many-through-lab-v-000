623bf8903181c50af8fbb287dfb2dac0a5f766b3
Peter Kim <peterckim96@gmail.com>
2018-06-09 22:26:09 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,22 @@
+class Genre
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+  
+  def artists
+    songs.map do |song|
+      song.artist
+    end
+  end
+  
+end
