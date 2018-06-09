ebc9bf23a198638125218a9c85c84f0dec4fab82
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-09 09:06:12 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,30 @@
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, artist)
+    song = Song.new(name, artist, genre=self)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    songs.map do |song|
+      song.artist
+    end
+  end
+end
