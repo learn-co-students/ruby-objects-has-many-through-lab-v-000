9ac947e9002b399e78adf0e33aab2ece8681c8e9
Megan Collier <meganraec@live.com>
2018-06-12 05:35:30 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select do |appt|
+      appt.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |dr|
+      dr.patient
+    end
+  end
+end
