b10537860fe96d25d41664931c426a4db323caa8
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-15 23:18:58 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+
+  @@all = []
+
+  attr_reader :genre
+  attr_accessor :artist
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
