b85e18cddfa418088bba681a93a404a2bcbcffb2
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-21 21:45:17 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,13 +14,12 @@ class Artist
   end
   
   def new_song(name,genre)
-    @songs << name 
-    name.artist = self 
+    song = Song.new(name,self,genre)
   end
   
   
   def songs 
-   binding.pry 
+   Song.all.select {|song| song.artist == self}
  end 
  
  
