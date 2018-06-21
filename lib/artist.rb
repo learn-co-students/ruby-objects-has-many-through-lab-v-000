255ea4d8c2b958e89b43e06f2ad559ea8b2c84dd
Richard Fenix <richard.fenix@gmail.com>
2018-06-21 18:57:56 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(song_name, genre)
+    Song.new(song_name, self, genre)
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def genres
+    songs.map {|song| song.genre}
+  end
+  
+end
+
+
+
+
+
+
+
+
+
+
