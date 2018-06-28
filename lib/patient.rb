4bc112d97b5599a9b12d50416494d20db2f5729f
Laura Messa Travin <laura.travin@gmail.com>
2018-06-28 06:02:41 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,10 +8,15 @@ class Patient
     def self.all
       @@all
     end
-    # def songs
-    #   Song.all.select { |e| e.genre =  self} #select requires a condition
-    # end
-    # def artists
-    #   Song.all.collect { |e| e.artist}  #map or collet return just values
-    # end
+     def new_appointment(doctor,date)
+    
+         Appointment.new(self,doctor,date)
+     end
+    def appointments
+
+       Appointment.all.select {|e| e.patient = self}  #get the app of the patient with all data
+    end
+    def doctors
+      appointments.map { |e| e.doctor}
+    end
 end
