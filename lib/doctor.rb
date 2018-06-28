ca930e3de170ce925300141e8cbc962ab33c0de0
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-28 22:34:38 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,4 +10,15 @@ class Doctor
   def self.all
     @@all
   end
+
+  def new_appointment(date, patient_instance)
+    new.Appointment(date, patient_instance, self)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor = self
+    end
+  end
+
 end
