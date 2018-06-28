667b0be10e9413b7fe0450ad8602d3754b833960
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-28 22:30:51 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,13 @@
+class Doctor
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
---
+++ lib/patient.rb
@@ -0,0 +1,9 @@
+class Patient
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+end
