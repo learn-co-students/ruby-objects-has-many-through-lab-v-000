fc3948886488001bd71d8f28c778cc07e0435709
Catherine Bronzo <cbronzo@mac.com>
2018-07-05 20:52:35 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,27 @@
+class Genre
+ 
+ attr_accessor :name
+ 
+ @@all = []
+ 
+ def initialize(name)
+   @name = name
+   @@all << name
+ end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, artist)
+    Song.new(name, artist, self)
+  end
+  
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+  
+  def artists
+    songs.map {|song| song.artist}
+  end
+end
