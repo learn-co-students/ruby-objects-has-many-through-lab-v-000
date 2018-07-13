3c94620ebdbfe476eb3ab73c7bf11818ec82524a
Amit Singh <amit_nyc@live.com>
2018-07-13 18:02:56 +0000

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,21 @@
 class Appointment
 
-  attr_accessor :patient
+  attr_accessor :patient, :doctor, :date
 
 @@all = []
 
+def initialize(patient, doctor, date)
+  @patient = patient
+  @doctor = doctor
+  @date = date
+  @@all << self
+  patient.add_appointment(self)
 
+end
+
+
+def self.all
+  @@all
+end
 
 end
--- lib/patient.rb
+++ lib/patient.rb
@@ -4,7 +4,29 @@ class Patient
 
 def initialize(name)
   @name = name
-end 
+  @appointments = []
+end
+
+def appointments
+  @appointments
+end
+
+def add_appointment(appointment)
+  @appointments << appointment
+end
+
+
+def new_appointment(doctor, date)
+  appointment = Appointment.new(self, doctor, date)
+  @appointments << appointment
+  appointment
+end
+
+def doctors
+ @appointments.collect do |appointment|
+   appointment.doctor
+ end
+ end
 
 
 
