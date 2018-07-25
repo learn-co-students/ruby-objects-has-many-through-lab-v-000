7d1c22667350d305ff7eaa4175d8c01ebbc0141e
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-25 19:51:33 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,14 @@
+class Appointment
+  attr_accessor :date
+  attr_reader :doctor, :patient
+  @@all=[]
+  def initialize(patient, doctor, date)
+  @date=date
+  @patient=patient
+  @doctor=doctor
+  @@all << self
+end
+def self.all
+  @@all
+end
+end
---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    Song.all{|song| song.genre == self}
+  end
+
+  def artists
+    songs.map{|song| song.artist}
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+end
