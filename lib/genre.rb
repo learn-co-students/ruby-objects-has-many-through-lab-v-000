9abd1b0395c99c50a4234b65c3f794d279aebd9b
Stavroula Guerrero <4stacyg@optonline.net>
2018-07-27 06:18:15 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,22 @@
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+
+  def artists
+    Song.all.collect {|song| song.artist}
+  end
+end
---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    Appointment.all.collect {|appointment| appointment.doctor}
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+end
