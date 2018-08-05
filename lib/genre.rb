39697a3c583c0e42c8a875f868afaad591c939c7
Aziz Kosber <a.kosber@gmail.com>
2018-08-05 01:11:49 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,15 @@
+class Genre
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    Song.all.select { |song| song.genre == self }
+  end
+
+  def artists
+    songs.collect { |song| song.artist }
+  end
+end
