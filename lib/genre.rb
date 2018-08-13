349582289d86ec74a23642f0fd30789af75c863c
Jae Yong Kim <kim900180@gmail.com>
2018-08-13 16:30:58 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, artist)
+    Song.new(name, artist, self)
+  end
+
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+
+  def artists
+    songs.map {|song| song.artist}
+  end
+
+  def self.all
+    @@all
+  end
+end
