6480a64e6e166fbb3e3110f63b3a2ac57ef62d29
Sharmaine Requerme <srqrme@gmail.com>
2018-08-16 03:57:17 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,4 +13,16 @@ class Artist
     @@all
   end
 
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select{|song| song.artist == self}
+  end
+
+  def genres
+    songs.map{|song| song.genre}
+  end
+
 end
--- lib/genre.rb
+++ lib/genre.rb
@@ -13,4 +13,11 @@ class Genre
     @@all
   end
 
+  def songs
+    Song.all.select{|song| song.genre == self}
+  end
+
+  def artists
+    songs.map{|song| song.artist}
+  end
 end
