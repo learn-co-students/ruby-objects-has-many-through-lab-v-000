df1547e35ad05fd51c53128d11546ba73882cd6f
Angie Kassab <anasab12@gmail.com>
2018-08-23 23:07:28 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -7,7 +7,26 @@ class Genre
   end 
   
   def initialize(name)
+    @name = name
     @@all << self
   end 
+  
+  def new_song(name, artist)
+    Song.new(name, artist, self) #self refers to artist because we are in the artist class, and the order of name-self-genre needs 
+    #to be in the same order as the initialize method in song.rb
+  end
+  
+  def songs
+    Song.all.select do |s|
+      s.genre == self #we're trying to match a genre to a genre, and we're in the Genre class
+    end 
+  end
+  
+  def artists
+    songs.collect do |genre| #songs - from the songs method above
+    genre.artist #returning the artist associated with that Genre
+    end
+  end 
+    
   
 end 
