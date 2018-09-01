154dcfcb205a7afe6400831aa50406907900e280
Shanise Barona <shanisebarona@gmail.com>
2018-09-01 20:42:01 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Doctor
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+  
+  def appointments
+    Appointment.all.select {|app| app.doctor == self}
+  end
+  
+  def patients 
+    appointments.map {|app| app.patient}
+  end
+  
+  def self.all 
+    @@all 
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Genre
+  attr_accessor :name
+  
+   @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+    def new_song(name, artist)
+    Song.new(name, self, artist)
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+  
+  def artists 
+    songs.map { |song| song.artist}
+  end
+end
