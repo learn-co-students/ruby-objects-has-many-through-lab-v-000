d9f2b0e9142e7b1c5d84070b95bc23b28c88b275
Rafael Lopez <lpz.raf@gmail.com>
2018-09-03 00:05:09 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,12 +5,14 @@ attr_accessor :name
 def initialize(name)
   @name = name
   @@all << self 
-  
 end
  
 def self.all 
     @@all 
 end
 
+def new_appointment(name, date)
+  Appointment.new(self, date)
+end
   
 end
