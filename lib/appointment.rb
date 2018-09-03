962e21d5238a19faa6b7caded5ad88b13a1460cd
Rafael Lopez <lpz.raf@gmail.com>
2018-09-03 00:09:13 +0000

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,17 @@
-# kjknkni
+class Appointment
+  @@all = []
+  
+  attr_accessor :date, :patient, :doctor
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+end
