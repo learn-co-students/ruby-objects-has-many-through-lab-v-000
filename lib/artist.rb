c758ed98ffbb69cff3534bfadf027b2aa40920f6
AT Williams <aderiannaw@yahoo.com>
2018-09-05 01:42:05 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,15 @@
+
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   
   def initialize(name)
     @name = name
     @songs = []
-    @genres = genres
-    
   end
   
   def add_song(song)
     @songs << song
     song.artist = self
-    binding.pry
   end
 
   def songs
@@ -19,6 +17,7 @@ class Artist
   end
 
   def genres
-    self.songs.collect {|song| song.genres}
+    self.songs.collect do |song| song.genre
   end
+end
 end
