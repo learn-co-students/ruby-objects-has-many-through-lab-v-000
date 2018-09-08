d66505c382d5c5aba5e4e6f9c4078b681610c5c9
Harley Rayner <harleymteden@gmail.com>
2018-09-08 04:51:18 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+    song.
+  end
+
+end
