3eba50c2212c934a979d788e14f59b6039496e99
maxwellbenton <maxwellbenton@gmail.com>
2018-09-10 11:45:16 -0400

    rebase from master, updated solution

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_hi
+    puts 'hi'
+  end
+end
