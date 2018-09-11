4550efd58f59472be36c3bd0858f12604d6112c9
Joann Pan <joannlpan@gmail.com>
2018-09-11 04:38:03 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Genre
+  attr_accessor :song
+  attr_reader :name, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    Song.all.map do |song|
+      song.artist
+    end
+  end
+end
