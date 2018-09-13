247ff2419da8d044e294166b8691e8112d9c9ac1
Claire Harmer <claire.f.harmer@gmail.com>
2018-09-13 01:18:34 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+class Patient
+  attr_accessor :name, :doctor, :appointment
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+    appointment.patient == self
+    end
+  end
+
+  def doctors
+    appointments.map do |appointment|
+    appointment.doctor
+    end
+  end
+end
