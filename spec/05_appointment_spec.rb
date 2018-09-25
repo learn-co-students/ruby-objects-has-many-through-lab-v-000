8a24243d988665b5e31295aa1e02e723df29af15
Ashley Bland <ashleyjbland@gmail.com>
2018-09-25 23:03:41 +0000

    Done.

--- spec/05_appointment_spec.rb
+++ spec/05_appointment_spec.rb
@@ -15,11 +15,11 @@ describe "Appointment" do
   end
 
   describe "#new" do
-    it "initializes with a patient, doctor, and date" do
+    it "initializes with a patient, date, and doctor" do
       doctor_who = Doctor.new("The Doctor")
       hevydevy = Patient.new("Devin Townsend")
 
-      expect{Appointment.new(hevydevy, doctor_who, "Friday, January 32nd")}.to_not raise_error
+      expect{Appointment.new(hevydevy, "Friday, January 32nd", doctor_who)}.to_not raise_error
     end
   end
 
