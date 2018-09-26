73f77ff45704be0baa315c6439d1685ebe1b4bc6
Sherry Martin <sm674@cornell.edu>
2018-09-26 21:57:08 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist 
+  @@all = []
+  
+  
+  def self.all
+    @@all << self
+  end
+  
+  def initialize(name)
+    @@all << @name 
+  end
+  
+  def new_song(name, genre)
+  artist.song
+  
+end
+end
