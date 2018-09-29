3df4c9370d7194487037617c83ce9f1c09f8aa49
John Werenski <jtwerenski@gmail.com>
2018-09-29 20:13:03 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  attr_accessor :genre, :artist
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
