16ed5e2f436e5d05f3508b9e53bd592e1fe1a711
Ashley Wong <ashwong04@gmail.com>
2018-09-30 00:11:12 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -15,10 +15,14 @@ class Genre
   end
   
   def songs
-    
+    Song.all.select do |song|
+      song.genre == self
+    end
   end
   
-  
+  def artists
+    
+  end
   
   
   
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   
- attr_reader :name, :artist, :genre
+ attr_accessor :name, :artist, :genre
   
   @@all = []
   
@@ -15,6 +15,5 @@ class Song
    @@all
   end
 
-  
   
 end
