aec5ee1c81dacda78e0f176e85b8dbe8cf4273f2
Rachel Wyatt <rw126@evansville.edu>
2018-10-03 23:44:14 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,23 +1,31 @@
+require "pry"
 class Patient
+  @@all = []
   attr_accessor :name
 
   def initialize(name)
     @name = name
-    @appointments = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def new_appointment(doctor, date)
-    @appointments << appointment
-    appointment.patient = self
+    Appointment.new(self, doctor, date)
   end
 
   def appointments
-    @appointments
+    Appointment.all.select do |appointment|
+    appointment.patient == self
+  end
   end
 
   def doctors
-    @appointments.collect do |appointment|
+    self.appointments.map do |appointment|
       appointment.doctor
     end
   end
+
 end
