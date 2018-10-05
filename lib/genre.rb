21275d8d7dc1575a5947e953362fd2c538db45fa
Miriam Castillo <castillo.m.mc@gmail.com>
2018-10-05 03:29:36 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,24 @@
+class Genre
+
+attr_accessor :name
+
+@@all = []
+
+ def initialize(name)
+  @name = name
+  @@all << self
+end
+
+ def self.all
+  @@all
+end
+
+ def songs
+  Song.all
+end
+
+ def artists
+  Song.all.map{|song| song.artist}
+end
+
+end
