d8369e28ad93db98fb6e038f14714af659034b03
Fatama Rahman <fatamarahman@gmail.com>
2018-10-08 22:31:11 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all { |song| song.genre == self }
+  end
+
+  def artists
+    songs.map(&:artist)
+  end
+
+
+end
