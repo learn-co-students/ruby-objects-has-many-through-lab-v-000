33329b8dfc6f998704593a72171a9351a5bf95ae
Susan Meyers <smmeyers24@gmail.com>
2018-10-09 01:23:39 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,21 @@
 class Artist
+  @@all = []
+  
+  attr_reader :name, :genre
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def new_song(name, genre)
+    
+    
+  end
   
 end
