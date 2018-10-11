118a1cbf9de2387e91ce8438856ceb873cbb98d8
Matt Etress <mattetress@me.com>
2018-10-11 03:37:55 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    songs.collect {|song| song.genre}
+  end
+
+
+
+end
