951d0e1191c7fc4db18edb6bb3886c2e58567105
hugo valois <hvgaming@yahoo.com>
2018-10-22 21:35:11 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Doctor
+  attr_accessor :name 
+ @@all = []
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all<< self
+  end
+  def self.all 
+    @@all
+  end
+  
+def new_appointment(patient,date)
+  app = Appointment.new(patient,date)
+   @appointments << app 
+     app.doctor = self
+     app
+end
+  
+  
+  def appointments
+    @appointments
+    end
+ 
+  def patients
+    appointments.map { |appointment| appointment.patient}
+  end
+  
+  
+end
