441ffdc970173c7f661636f66143e14df029924d
Vince Andrew Pimentel <contact@vincep.me>
2018-10-26 02:08:25 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,22 @@
+class Genre
+  attr_accessor :name
+
+  @@all = Array.new
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select { |song| song.genre == self }
+  end
+
+  def artists
+    songs.map { |song| song.artist }
+  end
+end
