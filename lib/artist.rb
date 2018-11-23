67e396b9ebdec0f2a761dffaabc7b57ac51ca01b
Lois Lee <lois.lee@me.com>
2018-11-23 20:53:41 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |sng|
+      sng.artist == self
+    end
+  end
+
+  def genres
+    songs.map do |sng|
+      sng.genre
+    end
+  end
+  
+end
