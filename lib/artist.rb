4b9bdd873d611101f921e2b0f6c446ccbc31093e
Paula McKinley <pmknyc@optimum.net>
2018-12-13 21:28:10 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class assert_instance_of
+ 
+ attr_accessor :name, 
+ @@all = []
+ 
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+   
+  end
+
+  def self.all
+    @@all 
+  end
+  
+  def new_song(name, genre)
+    Song.new(name, genre)
+    song.artist = self #tell song it belongs to this artist
+  end
+  
+  def songs 
+    Song.all.select do |song|
+      song.artist == self
+  end
+  
+  def genres 
+    self.songs.genre
+  end
+  
+end  #class end
