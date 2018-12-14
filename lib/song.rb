311b11f10c585d488630f518db06b17f175df03c
Ashleigh Chun <ashleigh.chun@wework.com>
2018-12-14 00:36:05 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+require "pry"
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@all = []
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    @@all << self
+
+  end
+  
+  def self.all 
+    @@all 
+    
+  end
+  
+end 
+
+
+#genre = waiter
+#song = meal
+#artist = customer
