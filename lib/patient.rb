d272d4f36b76c6d7a13eab141294a71bae48f770
Paula McKinley <pmknyc@optimum.net>
2018-12-15 00:17:57 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Patient
+
+	attr_accessor :name
+	@@all = []
+
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def new_appointment(date, doctor)
+		Appointment.new(date, self, doctor)
+	end
+
+	def appointments
+		Appointment.all.collect do
+			|appt| appt.patient == self
+		end
+	end
+
+	def doctors
+		self.appointments.collect do
+			|appt| appt.doctor
+		end
+	end
+end #class end
