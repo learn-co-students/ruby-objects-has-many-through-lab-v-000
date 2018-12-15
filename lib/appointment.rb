f5cb0932d534052315926a63aba97a9448892245
Paula McKinley <pmknyc@optimum.net>
2018-12-15 00:09:49 +0000

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,31 +1,15 @@
 require 'pry'
 
-class Doctor
+class Appointment
 
-	attr_accessor :name
+	attr_accessor :date, :patient, :doctor
 	@@all = []
 
-	def initialize(name)
-		@name = name
+	def initialize(date, patient, doctor)
+		@date = date
+		@patient = patient
+		@doctor = doctor
 		@@all << self
 	end
 
-	def new_appointment(date, patient)
-		Appointment.new(date, patient, self)
-	end
-
-# create array of a doctor's appts
-	def appointments
-		Appointment.all.collect do
-			|appt| appt.doctor == self
-		end
-	end
-
-# array of patients for all a doc's appts
-	def patients
-		self.appointments.collect do
-			|appt| appt.patient
-		end
-	end
-
-end  # class end
+	end # class end
