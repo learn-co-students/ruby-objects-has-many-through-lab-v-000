961197522a9e0b266e917402f06ad5a714062e66
Sarah Paz <sarah.paz@gmail.com>
2019-02-02 07:14:29 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  attr_accessor :name, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Somg.new(name, self, genre)
+  end
+end
