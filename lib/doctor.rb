2ea2caf89a7fd4be45adaa804f58a0881640714b
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-18 04:10:42 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+  attr_accessor :name, :patients, :appointments
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    appointment = Appointment.new(date, patient, self)
+    @appointments << appointment
+    appointment
+  end
+
+  def appointments
+    @appointments.select {|appointment| appointment.doctor}
+  end
+
+  def patients
+    @appointments.collect {|appointment| appointment.patient}
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
+
+# - The `Song` class needs a class variable `@@all` that begins as an empty array.
+# - The `Song` class needs a class method `.all` that lists each song in the class
+#   variable.
+# - A song should be initialized with a name, an artist, and a genre, and be saved
+#   in the `@@all` array.
