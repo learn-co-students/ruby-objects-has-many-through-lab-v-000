a61ebf46634e40125d5b376b7bf490a9fafe35f9
Erica Murdock-Waters <ericahpa@gmail.com>
2019-02-19 18:54:59 +0000

    Revised order of #new_appointment parameters

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+# artist.#!/usr/bin/env ruby -wKU
+class Artist
+  attr_accessor :name, :genre
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(song, genre)
+    Song.new(song, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.map {|song| song.genre}
+  end
+
+end
