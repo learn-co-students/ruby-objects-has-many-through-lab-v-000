c0632bf86b4139bd15534c6d2842f2a986a7d914
Niusha H <niusha.hadziomerovic@gmail.com>
2019-03-01 21:08:57 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,7 @@ class Artist
   end 
   
   def new_song(song_name, genre)
-    Artist.new(song_name, genre)
+    Song.new(song_name, genre)
     # @genre = []
     # @new_song = [song_name, genre] 
   end
