5ba34b46ce2884e35982f646dfb2042e217f9d54
Kimberly Aviles <01010110.v@gmail.com>
2019-03-02 07:01:54 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,22 +2,29 @@ class Artist
   attr_accessor :name, :songs, :genres
   @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self #<---usually replaced with a #SAVE <---best way
+  end
+
   def self.all
     @@all
   end
 
-  def initialize(name)
-    @name = name
-    @songs = []
-
-    @@all << self
-  end
-
   def new_song(name, genre)
     Song.new(name, self, genre)
-    self.songs << self
+  end
 
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
 
+  def genres
+    songs.collect do |song|
+      song.genre
+    end
   end
 
 end #<----- CLASS end
--- lib/genre.rb
+++ lib/genre.rb
@@ -2,14 +2,24 @@ class Genre
   attr_accessor :name, :songs, :artists
   @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
   def self.all
     @@all
   end
 
-  def initialize(name)
-    @name = name
-    @songs = []
-    self.class.all << self
+  def songs
+    Song.all.select { |song| song.genre == self }
+    #grab all the SONG OBJECTS that their genres match to the one being called on
+  end
+
+  def artists
+    songs.collect { |song| song.artist }
+    #refer to the #SONGS method which pulls relevent songs to the genre
+    #and return the relevent ARTISTs
   end
 
 end #<---- CLASS end
