907c43162123d25b6a3fbbf8535f0e2be38a34f4
Kimberly Aviles <01010110.v@gmail.com>
2019-03-02 07:42:39 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,22 @@
 class Doctor
-  attr_accessor :apppointments
+  #attr_accessor
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient)
+  end
+
+  def appointments
   end
 
 end #<---- CLASS end
