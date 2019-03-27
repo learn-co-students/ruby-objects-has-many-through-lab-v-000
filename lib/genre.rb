f4902fa65cd4453c4da43eb79fa90de306cda6e0
David Forer <david.j.forer@gmail.com>
2019-03-27 18:29:18 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre 
+  attr_accessor :name 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+
+   def self.all 
+    @@all 
+  end 
+
+   def songs 
+    Song.all.select {|s| s.genre == self} 
+  end 
+
+   def artists
+    songs.map {|s| s.artist} 
+  end 
+end
