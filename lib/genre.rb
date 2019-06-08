6267fa2fd96675048ae858107fe1018f5e83ca43
Dan Suarez <suarez.daniel101@gmail.com>
2019-06-08 21:39:51 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,11 @@
 class Genre
-  attr_accessor :name
+  attr_accessor :artist, :song, :name
 
   @@all = []
 
-  def initialize(name)
+  def initialize(artist, song, name)
+    @artist = artist
+    @song = song
     @name = name
     @@all << self
   end
@@ -11,5 +13,5 @@ class Genre
   def self.all
     @@all
   end
-  
+
 end
