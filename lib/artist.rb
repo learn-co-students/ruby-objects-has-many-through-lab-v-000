ae2ab722e9db3716daa3e95d9401beeda9247270
Dan Suarez <suarez.daniel101@gmail.com>
2019-06-09 19:45:06 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,9 +15,7 @@ class Artist
   end
 
   def new_song(name, genre)
-    song = Song.new(name, artist, genre)
-    song.artist == self
-
+    Song.new(name, self, genre)
   end
 
   def songs
@@ -25,5 +23,6 @@ class Artist
       song.artist == self
     end
   end
+
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
     @@all
   end
 
-  def genre
-    
-  end
+  # def genre
+  #
+  # end
 end
