e4a572740c0e952ba6130788267dc8204894037b
Tam Vu <vutam_a@hotmail.com>
2019-07-03 22:57:48 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def new_song(song, genre)
+    Song.new(song, self, genre)
+  end 
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def genres
+    songs.collect {|song| song.genre}
+  end 
+end 
