8fc846769961fdfb423af61e6290cdcf7f6799c4
maxwellbenton <maxwellbenton@gmail.com>
2019-07-26 16:42:57 -0400

    updates appointments test to not rely on new_appt

--- spec/06_patient_spec.rb
+++ spec/06_patient_spec.rb
@@ -29,15 +29,19 @@ describe 'Patient' do
   end
 
   describe '#appointments' do
-    it 'has many appointments' do
-      doctor_dog = Doctor.new('Dog')
-      doctor_howser = Doctor.new('Howser')
-      steve = Patient.new('Steve from Blues Clues')
-      appointment_one = steve.new_appointment(doctor_dog, 'Friday, January 32nd')
-      appointment_two = steve.new_appointment(doctor_howser, 'Saturday, January 32nd')
+    it 'returns all appointments associated with this Patient' do
+      doctor_who = Doctor.new('The Doctor')
+      doctor_smith = Doctor.new('Matt Smith')
+      steve = Patient.new('Steve')
+      mike = Patient.new('Mike')
+      appointment = Appointment.new(steve, doctor_who, 'Friday, January 32nd')
+      appointment_2 = Appointment.new(steve, doctor_smith, 'Saturday, January 33rd')
+      appointment_3 = Appointment.new(mike, doctor_who, "Sunday, January 34th")
 
-      expect(steve.appointments).to include(appointment_one)
-      expect(steve.appointments).to include(appointment_two)
+      expect(steve.appointments).to include(appointment)
+      expect(steve.appointments).to include(appointment_2)
+      expect(steve.appointments).to_not include(appointment_3)
+      expect(mike.appointments).to include(appointment_3)
     end
   end
 
