6e4d85cf94cd3807dfb26f1f210ba357fa3456ae
maxwellbenton <maxwellbenton@gmail.com>
2019-07-31 16:31:32 -0400

    aligns appointment tests to lesson instructions

--- spec/04_doctor_spec.rb
+++ spec/04_doctor_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-
+require 'pry'
 describe 'Doctor' do
   describe '#name' do
     it 'has a name' do
@@ -21,8 +21,8 @@ describe 'Doctor' do
       doctor_who = Doctor.new('The Doctor')
       doctor_smith = Doctor.new('Matt Smith')
       hevydevy = Patient.new('Devin Townsend')
-      appointment = Appointment.new(hevydevy, doctor_who, 'Friday, January 32nd')
-      appointment_2 = Appointment.new(hevydevy, doctor_smith, 'Saturday, January 33rd')
+      appointment = Appointment.new('Friday, January 32nd', hevydevy, doctor_who)
+      appointment_2 = Appointment.new('Saturday, January 33rd', hevydevy, doctor_smith)
       
       expect(doctor_who.appointments).to include(appointment)
       expect(doctor_smith.appointments).to include(appointment_2)
--- spec/06_patient_spec.rb
+++ spec/06_patient_spec.rb
@@ -34,9 +34,9 @@ describe 'Patient' do
       doctor_smith = Doctor.new('Matt Smith')
       steve = Patient.new('Steve')
       mike = Patient.new('Mike')
-      appointment = Appointment.new(steve, doctor_who, 'Friday, January 32nd')
-      appointment_2 = Appointment.new(steve, doctor_smith, 'Saturday, January 33rd')
-      appointment_3 = Appointment.new(mike, doctor_who, "Sunday, January 34th")
+      appointment = Appointment.new('Friday, January 32nd', steve, doctor_who)
+      appointment_2 = Appointment.new('Saturday, January 33rd', steve, doctor_smith)
+      appointment_3 = Appointment.new('Sunday, January 34th', mike, doctor_who)
 
       expect(steve.appointments).to include(appointment)
       expect(steve.appointments).to include(appointment_2)
