b1e8ce0bfa49c1092f967415465032fdc9ea2893
maxwellbenton <maxwellbenton@gmail.com>
2019-07-31 16:34:06 -0400

    aligns solution/readme appointments#initialization

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ class Appointment
 
   @@all = []
 
-  def initialize(patient, doctor, date)
+  def initialize(date, patient, doctor)
     @date = date
     @doctor = doctor
     @patient = patient
--- lib/patient.rb
+++ lib/patient.rb
@@ -13,7 +13,7 @@ class Patient
   end
 
   def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
+    Appointment.new(date, self, doctor)
   end
 
   def appointments
