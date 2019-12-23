a9a63daa4a41d302375c9081bb9d1462e3fa7896
maxwellbenton <maxwellbenton@gmail.com>
2019-12-23 15:36:01 -0500

    readds tests to cover false positives

--- spec/01_artist_spec.rb
+++ spec/01_artist_spec.rb
@@ -25,10 +25,13 @@ describe "Artist" do
     it "returns all songs associated with this Artist" do
       jay_z = Artist.new("Jay-Z")
       rap = Genre.new("rap")
-
+      tmbg = Artist.new("They Might Be Giants")
+      nerd_rock = Genre.new("Nerd Rock")
       ninety_nine_problems = Song.new("Ninety Nine Problems", jay_z, rap)
+      particle_man = Song.new("Particle Man", tmbg, nerd_rock)
 
       expect(jay_z.songs).to include(ninety_nine_problems)
+      expect(jay_z.songs).not_to include(particle_man)
     end
   end
 
@@ -36,9 +39,11 @@ describe "Artist" do
     it "given a name and genre, creates a new song associated with that artist" do
       jay_z = Artist.new("Jay-Z")
       rap = Genre.new("rap")
+      nerd_rock = Genre.new("Nerd Rock")
       ninety_nine_problems = jay_z.new_song("Ninety Nine Problems", rap)
 
       expect(jay_z.songs).to include(ninety_nine_problems)
+      expect(jay_z.genres).not_to include(nerd_rock)
       expect(ninety_nine_problems.artist).to eq(jay_z)
     end
   end
