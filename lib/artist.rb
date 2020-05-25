12b0695b3acee40d17b788ab9be9346b830c8e38
Richard Hanolt <rhanolt@gmail.com>
2020-05-25 03:58:05 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select{|song| song.artist == self}
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def genres
+    songs.collect{|song| song.genre}
+  end
+
+end
