63e48e028679606132d990621ade7da56a02c8f4
idith almog <almog.idith@gmail.com>
2020-05-28 19:25:46 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,20 @@
+class Patient
+  attr_accessor :name
+  @@all =[]
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_appointment(date,doctor)
+    Appointment.new(date,self,doctor)
+  end
+  def appointments
+    Appointment.all.select {|appt| appt.patient == self}
+  end
+  def doctors
+    self.appointments.map {|appt| appt.doctor}
+  end
+end
