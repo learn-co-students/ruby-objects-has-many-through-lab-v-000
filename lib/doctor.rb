fcbfda1b20e6385362cb92505e98bba41af8259d
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-06-25 04:57:53 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def patients
+    appointments.map do |appointment|
+      appointment.patient
+    end
+  end
+end
