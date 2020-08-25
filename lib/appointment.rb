5fe504d034ea62ca3bb5001b80b8c0069dc2d4fc
Jaime Johnsen <drivetraindiva@gmail.com>
2020-08-25 00:58:30 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,14 @@
+class Appointment
+  attr_accessor :date, :doctor
+
+  @all = []
+
+  def self.all
+    @@all
+  end
+
+  #def initialize(date"Monday, August 1st", doctor)
+  #
+  #end
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,29 @@
 class Patient
+  attr_accessor :name
+
+  @@all =[]
+
+  def self.all
+    @@all
+  end
+
+#  def initialize(name)
+#    @name = name
+#  end
 
 
+# new_appointment(date, Doctor)
+# @date = date
+# @new_appointment = new appointment
+#
+# end
+
+# def appointments
+# @appointments = apointment
+#
+# end
+
+# def doctors
+#
+# end
 end
