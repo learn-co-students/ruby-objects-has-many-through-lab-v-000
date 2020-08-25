6d59baecca78f752f651e3bf4045a2f1271654c3
Jaime Johnsen <drivetraindiva@gmail.com>
2020-08-25 00:17:32 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+end
